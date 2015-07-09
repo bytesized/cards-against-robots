c078ccb7925690465b35728695286d87441c4a1d
bytesized <bytesized@users.noreply.github.com>
2015-07-09 00:15:25 -0700

    Added is_not validator to allow certain inputs to be prevented

--- custom_validators.js
+++ custom_validators.js
@@ -3,7 +3,7 @@ var validator = require('validator');
 module.exports = {
 	custom_int: function(param, options)
 	{
-		param = validator.toInt(param);
+		param = validator.toInt(param, 10);
 		if (isNaN(param))
 			return false;
 		if (options.positive && param <= 0)
@@ -11,5 +11,17 @@ module.exports = {
 		if (options.even && param % 2 != 0)
 			return false;
 		return true;
+	},
+	is_not: function(param, comparison, case_insensitive)
+	{
+		if (case_insensitive)
+		{
+			param = param.toLowerCase();
+			comparison = comparison.toLowerCase();
+		}
+		if (param == comparison)
+			return false;
+		else
+			return true;
 	}
 };
