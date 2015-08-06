693fae3de1cf9d52a1924b877279dfed3c7c4a6b
bytesized <bytesized@users.noreply.github.com>
2015-08-06 10:46:39 -0700

    Added socket.io. It is supported in such a way that it can be accessed from any file via the common/socket_io module and is initialized before any code that might need it has a chance to run

--- app.js
+++ app.js
@@ -1,109 +1,107 @@
 "use strict";
-// Promisification (only needs to be done once)
-var Promise = require("bluebird");
-Promise.promisifyAll(require("mysql/lib/Connection").prototype);
-Promise.promisifyAll(require("mysql/lib/Pool").prototype);
-Promise.promisifyAll(require('bcrypt'));
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var stylus = require('stylus');
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var express_validator = require('express-validator');
-var secret = require(path.join(__dirname, 'common', 'secret'));
-var config = require(path.join(__dirname, 'configuration'));
-var app_config = {};
-app_config.validator = require(path.join(__dirname, 'config', 'validator'));
-app_config.passport = require(path.join(__dirname, 'config', 'passport'));
-app_config.stylus = require(path.join(__dirname, 'config', 'stylus'));
-app_config.routes = require(path.join(__dirname, 'config', 'routes'));
-app_config.config_routes = require(path.join(__dirname, 'config', 'config_routes'));
-app_config.flash = require(path.join(__dirname, 'config', 'flash'));
-app_config.configuration = require(path.join(__dirname, 'config', 'configuration'));
-// Some modules should be initialized when the server starts, but aren't actually needed
-// in this file
-require(path.join(__dirname, 'common', 'words'));
-require(path.join(__dirname, 'common', 'room_name'));
+module.exports = function(app, server)
+{
+	var path = require('path');
 
-var app = express();
+	// Initialize socket.io before ANYTHING else so that everything else has access to it
+	require(path.join(__dirname, 'common', 'socket_io')).init(server);
 
-var args = process.argv.slice(2);
+	// Promisification (only needs to be done once)
+	var Promise = require("bluebird");
+	Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+	Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+	Promise.promisifyAll(require('bcrypt'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+	var favicon = require('serve-favicon');
+	var express = require('express');
+	var logger = require('morgan');
+	var cookieParser = require('cookie-parser');
+	var bodyParser = require('body-parser');
+	var stylus = require('stylus');
+	var passport = require('passport');
+	var flash = require('connect-flash');
+	var express_validator = require('express-validator');
+	var config = require(path.join(__dirname, 'configuration'));
+	var app_config = {};
+	app_config.validator = require(path.join(__dirname, 'config', 'validator'));
+	app_config.passport = require(path.join(__dirname, 'config', 'passport'));
+	app_config.stylus = require(path.join(__dirname, 'config', 'stylus'));
+	app_config.routes = require(path.join(__dirname, 'config', 'routes'));
+	app_config.config_routes = require(path.join(__dirname, 'config', 'config_routes'));
+	app_config.flash = require(path.join(__dirname, 'config', 'flash'));
+	app_config.configuration = require(path.join(__dirname, 'config', 'configuration'));
+	app_config.session_middleware = require(path.join(__dirname, 'config', 'session_middleware'));
+	// Some modules should be initialized when the server starts, but aren't actually needed
+	// in this file
+	require(path.join(__dirname, 'common', 'words'));
+	require(path.join(__dirname, 'common', 'room_name'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+	var args = process.argv.slice(2);
 
-// If the -r option is not specified, allow fallback to pseudorandom data
-var allow_random_fallback = (args.indexOf('-r') !== -1);
-app.use(session({
-	secret: secret.generate(256, allow_random_fallback).toString(),
-	saveUninitialized: true,
-	resave: true
-}));
+	// view engine setup
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'jade');
 
-app.use(passport.initialize());
-app.use(passport.session());
-app_config.passport();
+	// uncomment after placing your favicon in /public
+	//app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
+	app.use(logger('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+
+	app.use(app_config.session_middleware);
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app_config.passport();
 
 
-app.use(express_validator(app_config.validator.options));
-app.use(app_config.validator.middleware);
-app.use(cookieParser());
-app.use(stylus.middleware(app_config.stylus));
+	app.use(express_validator(app_config.validator.options));
+	app.use(app_config.validator.middleware);
+	app.use(cookieParser());
+	app.use(stylus.middleware(app_config.stylus));
 
-// Static routes
-app.use(express.static(path.join(__dirname, 'public')));
+	// Static routes
+	app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(flash());
-app.use(app_config.flash);
+	app.use(flash());
+	app.use(app_config.flash);
 
-// Sets `res.locals.config`
-app.use(app_config.configuration);
+	// Sets `res.locals.config`
+	app.use(app_config.configuration);
 
-// Routing - If the site is not configured yet, route configuration
-// page rather than regular website
-if (config.properties.is_configured)
-	app_config.routes(app);
-else
-	app_config.config_routes(app);
+	// Routing - If the site is not configured yet, route configuration
+	// page rather than regular website
+	if (config.properties.is_configured)
+		app_config.routes(app);
+	else
+		app_config.config_routes(app);
 
-// error handlers
+	// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+	// development error handler
+	// will print stacktrace
+	if (app.get('env') === 'development') {
+		app.use(function(err, req, res, next)
+		{
+			res.status(err.status || 500);
+			res.render('error', {
+				message: err.message,
+				error: err
+			});
+		});
+	}
+
+	// production error handler
+	// no stacktraces leaked to user
 	app.use(function(err, req, res, next)
 	{
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
-			error: err
+			error: {}
 		});
 	});
+
+	return app;
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next)
-{
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
-
-
-module.exports = app;
---
+++ common/socket_io.js
@@ -0,0 +1,62 @@
+"use strict";
+// Allows a more decentralized usage of socket.io
+// `init()` MUST be called before using
+var path = require('path');
+var session_middleware = require(path.normalize(path.join(__dirname, '..', 'config', 'session_middleware')));
+var user = require(path.normalize(path.join(__dirname, '..', 'db', 'user')));
+
+var io;
+
+var connect_authenticate_middleware = function(io_namespace)
+{
+	io_namespace.use(function(socket, next)
+	{
+		session_middleware(socket.request, {}, next);
+	}).use(function(socket, next)
+	{
+		// Reject anyone without a passport session
+		if (!socket.request.session.passport)
+			return next(new Error('Authentication Error - Session not found'));
+		if (socket.request.session.passport.user === undefined)
+			return next(new Error('Authentication Error - Session not found'));;
+
+		var user_id = socket.request.session.passport.user;
+		user.get_by_id(user_id).then(function(user_info)
+		{
+			if (user_info === null)
+			{
+				next(new Error('Authentication Error'));
+			} else
+			{
+				socket.request.user = user_info;
+				next();
+			}
+		}, function(err)
+		{
+			next(new Error('Authentication Error'));
+		});
+	});
+};
+var authenticate_middleware = function(socket, next)
+{
+	session_middleware(socket.request, {}, function(err)
+	{
+	});
+}
+
+var init = function(server)
+{
+	io = require('socket.io')(server);
+	connect_authenticate_middleware(io);
+};
+
+var get_io = function()
+{
+	return io;
+};
+
+module.exports = {
+	init                            : init,
+	io                              : get_io,
+	connect_authenticate_middleware : connect_authenticate_middleware
+};
